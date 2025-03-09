ad1a4dbe1c91e663beabedd196959f0cef36945a
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 13:07:40 -0300

    Add files via upload

---
+++ Exercicio3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] diasDaSemana = new string[8];
+            int numero;
+
+            diasDaSemana [0] = "Domingo";
+            diasDaSemana [1] = "Segunda-Feira";
+            diasDaSemana [2] = "Terça-Feira";
+            diasDaSemana [3] = "Quarta-Feira";
+            diasDaSemana [4] = "Quinta-Feira";
+            diasDaSemana [5] = "Sexta-Feira";
+            diasDaSemana [6] = "Sabado";
+
+            Console.WriteLine("Escreva um número inteiro entre 1 e 7:");
+
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 0 && numero <= 7)
+            {
+                Console.WriteLine("O dia da semana é:" + diasDaSemana[numero - 1]);
+            }
+
+            else
+            {
+                Console.WriteLine("Número inválido, tente um número entre 1 e 7");
+            }
+        }
+    }
+}
