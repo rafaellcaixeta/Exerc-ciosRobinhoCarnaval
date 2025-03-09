c3d1b10d81efc1358a980bc383528556693eb7a8
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 12:02:35 -0300

    Add files via upload

---
+++ Exercicio2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Altura, PesoIdeal;
+            string sexo;
+
+            Console.WriteLine("Digite o sexo da pessoa - Masculino (M) ou Feminino (F):");
+            sexo = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite a altura da pessoa:");
+            Altura = double.Parse(Console.ReadLine());
+
+            if (sexo == "M")
+            {
+                PesoIdeal = (72.7 * Altura) - 58;
+                Console.WriteLine($"O peso ideal é: {PesoIdeal:F2}kg.");
+            }
+            else if (sexo == "F") 
+            {
+                PesoIdeal = (62.1 * Altura) - 44.7;
+                Console.WriteLine($"O peso ideal é: {PesoIdeal:F2}kg.");
+            }
+        }
+    }
+}
