4d6ccba2f83c53a7da4d3b4c9b9221dca4306773
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 20:07:50 -0300

    Add files via upload

---
+++ Exercicio7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[5];
+            int[] y = new int[10];
+            int pontuacao = 0;
+
+            Console.WriteLine("Digite 5 números do sorteio da loto:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                x[i] = int.Parse(Console.ReadLine());   
+            }
+
+            Console.WriteLine("Digite 10 números da sua aposta:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                y[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int num in y)
+            {
+                if (Array.Exists(x, element => element == num))
+                {
+                    pontuacao++;
+                }
+            }
+            Console.WriteLine($"Você fez {pontuacao} pontos.");
+        }
+    }
+}
