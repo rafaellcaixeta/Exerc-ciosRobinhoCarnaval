e545827b818dab0df39fd64915b30bd4f4abccd1
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 20:32:15 -0300

    Add files via upload

---
+++ Exercicio8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] d = new int[20];
+            Console.WriteLine("Digite 20 numeros:");
+            
+            for (int i = 0; i < 20; i++)
+            {
+                d[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Digite N:");
+            int n = int.Parse(Console.ReadLine());
+            
+            List<int> compactado = new List<int>();
+
+            foreach (int item in d)
+            {
+                if (item > n)
+                {
+                    compactado.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(", ", compactado));
+        }
+    }
+}
