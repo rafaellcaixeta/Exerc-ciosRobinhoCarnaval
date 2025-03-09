27695d3bbf6670e6f29b9856f7fa52a36bd2d909
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 13:31:29 -0300

    Add files via upload

---
+++ Exercicio4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double TrabalhoSemanal = 40;
+            double HorasTrabalhadas;
+            double Salario = 1640;
+            double NovoSalario;
+            double HorasExtras;
+            double SalarioHora;
+            double SalarioExtra;
+
+            Console.WriteLine("Escreva as horas trabalhadas:");
+            HorasTrabalhadas = int.Parse(Console.ReadLine());
+
+
+
+            if (HorasTrabalhadas > 40)
+            {
+                HorasExtras = HorasTrabalhadas - TrabalhoSemanal;
+                SalarioHora = Salario / TrabalhoSemanal;
+                SalarioExtra = HorasExtras * SalarioHora * 1.5;
+                NovoSalario = Salario + SalarioExtra;
+
+                Console.WriteLine($"Salário é {NovoSalario:F2}");
+            }
+
+
+
+
+        }
+    }
+}
