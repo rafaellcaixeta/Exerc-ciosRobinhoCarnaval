f71cb75208da99e4fd86d1ad6c300c91e8c2b9fd
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 13:46:47 -0300

    Add files via upload

---
+++ Exercicio5.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Responda com 'sim' ou 'não'.");
+
+        Console.WriteLine("É mamífero? ");
+        string resposta = Console.ReadLine().ToLower();
+
+        if (resposta == "sim")
+        {
+            Console.WriteLine("É quadrúpede? ");
+            resposta = Console.ReadLine().ToLower();
+
+            if (resposta == "sim")
+            {
+                Console.WriteLine("É carnívoro? ");
+                resposta = Console.ReadLine().ToLower();
+
+                if (resposta == "sim")
+                {
+                    Console.WriteLine("O animal escolhido foi o leão.");
+                }
+                else
+                {
+                    Console.WriteLine("O animal escolhido foi o cavalo.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("É onívoro? ");
+                resposta = Console.ReadLine().ToLower();
+
+                if (resposta == "sim")
+                {
+                    Console.WriteLine("O animal escolhido foi o homem.");
+                }
+                else
+                {
+                    Console.WriteLine("O animal escolhido foi o macaco.");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("É uma ave? ");
+            resposta = Console.ReadLine().ToLower();
+
+            if (resposta == "sim")
+            {
+                Console.WriteLine("É uma ave não-voadora? ");
+                resposta = Console.ReadLine().ToLower();
+
+                if (resposta == "sim")
+                {
+                    Console.WriteLine("É tropical? ");
+                    resposta = Console.ReadLine().ToLower();
+
+                    if (resposta == "sim")
+                    {
+                        Console.WriteLine("O animal escolhido foi o avestruz.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O animal escolhido foi o pinguim.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("É uma ave nadadora? ");
+                    resposta = Console.ReadLine().ToLower();
+
+                    if (resposta == "sim")
+                    {
+                        Console.WriteLine("O animal escolhido foi o pato.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O animal escolhido foi a águia.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("O animal escolhido foi a baleia.");
+            }
+        }
+    }
+}
