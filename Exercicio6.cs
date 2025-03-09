749953a367df4b8d6ef82cbfcd918b566b24fec1
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 17:11:30 -0300

    Add files via upload

---
+++ Exercicio6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double S = 1.0;
+        int numerador = 3;
+        int denominador = 2;
+
+        while (numerador <= 99 && denominador <= 50)
+        {
+            S += (double)numerador / denominador;
+            numerador += 2;
+            denominador++;
+        }
+
+        Console.WriteLine($"O valor de S é: {S:F2}");
+    }
+}
