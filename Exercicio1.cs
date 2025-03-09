21c97756e83bf493ade86e18e90cb82179d4e0de
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-09 12:02:11 -0300

    Add files via upload

---
+++ Exercicio1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Myapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int VendaMediaMensal;
+            double precoAtual = 30;
+            double novoPreco;
+
+            Console.WriteLine("Qual foi a média de venda mensal?");
+            VendaMediaMensal = int.Parse(Console.ReadLine());
+
+            if (VendaMediaMensal< 500)
+            {
+                novoPreco = precoAtual * 1.10;
+
+                Console.WriteLine("O novo valor é:" + novoPreco);
+            }
+            else
+            {
+                novoPreco = precoAtual * 0.80;
+
+                Console.WriteLine("O novo valor é:" + novoPreco);
+            }
+        }
+    }
+}
